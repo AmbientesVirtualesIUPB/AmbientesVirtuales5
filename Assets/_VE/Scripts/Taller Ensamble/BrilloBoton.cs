90816a3007e1ae56fa3a337b723558104af7ae44
JuliánMolinaJaramillo <judmolin@gmail.com>
2025-01-20 00:46:10 -0500

    Se comienza a pulir la interfaz y la botonera

---
+++ Assets/_VE/Scripts/Taller Ensamble/BrilloBoton.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class BrilloBoton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
+{
+    public GameObject imagenBrillo; // Asigna la imagen del brillo
+    public GameObject imagenIcono, imagenIconoFurtivo;
+    public Sprite icono;
+
+    private void Start()
+    {
+        if (imagenBrillo != null)
+        {
+            imagenBrillo.SetActive(false); // Nos aseguramos de que el brillo esté apagado al inicio
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (imagenBrillo != null)
+        {
+            imagenBrillo.SetActive(true); // Activa el brillo al resaltar
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (imagenBrillo != null)
+        {
+            imagenBrillo.SetActive(false); // Desactiva el brillo al salir
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (imagenIcono != null)
+        {
+            imagenIcono.SetActive(true);
+            imagenIconoFurtivo.SetActive(true);
+
+            Image image = imagenIcono.GetComponent<Image>();
+            Image image2 = imagenIconoFurtivo.GetComponent<Image>();
+
+            if (image != null)
+            {
+                image.sprite = icono;
+                image2.sprite = icono;
+            }
+            else
+            {
+                Debug.LogError("El GameObject no tiene un componente Image.");
+            }
+        }
+    }
+
+}
