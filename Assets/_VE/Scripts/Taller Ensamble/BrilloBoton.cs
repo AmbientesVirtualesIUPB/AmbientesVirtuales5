c0938649a49050632c5feb068a27c412355b3997
Julian Molina <judmolin@gmail.com>
2025-01-21 11:23:13 -0500

    Se mejora la interfaz de forma visual

--- Assets/_VE/Scripts/Taller Ensamble/BrilloBoton.cs
+++ Assets/_VE/Scripts/Taller Ensamble/BrilloBoton.cs
@@ -5,16 +5,21 @@ using UnityEngine.UI;
 
 public class BrilloBoton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
 {
-    public GameObject imagenBrillo; // Asigna la imagen del brillo
-    public GameObject imagenIcono, imagenIconoFurtivo;
-    public Sprite icono; 
-    public bool esBoton;
-
-    [HideInInspector]
-    public bool botonPresionado;
+    public RectTransform botonPersonalizacion; // Referecias solo para los botones de guardado de personalizacion
+    public GameObject imagenBrillo, brilloIcono; // El brillo del boton en accion, y el brillo de los iconos del panel informativo
+    public Image imagenIcono, imagenIconoFurtivo; // Icono del panel informativo e icono del canvas furtivo sobre el vehiculo
+    public Sprite icono; // Imagen a la que queremos cambiar los iconos
+    public bool esBoton, esPanel; // Para validar si es un boton con cambio de icono, o es un boton del panel informativo
+    public float valorAlfa; // Para variar el cambio del valor alfa en todos los botones
+    private Vector3 posicionInicialBoton; // Para guardar la escala inicial de los botones de personalizacion
 
     private void Start()
     {
+        if (botonPersonalizacion != null)
+        {
+            posicionInicialBoton = botonPersonalizacion.localScale; // Guardamos la escala inicial
+        }
+        
         if (imagenBrillo != null)
         {
             imagenBrillo.SetActive(false); // Nos aseguramos de que el brillo esté apagado al inicio
@@ -26,6 +31,24 @@ public class BrilloBoton : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
         if (imagenBrillo != null)
         {
             imagenBrillo.SetActive(true); // Activa el brillo al resaltar
+
+            Image imagenActual = this.gameObject.GetComponent<Image>(); // Obtenemos el componenete imagen de nuestro objeto actual
+            Color colorActual = imagenActual.color; // Obtenemos el color actual de la imagen
+            colorActual.a = 1f; // Seteamos el alfa en 1
+            imagenActual.color = colorActual; // Asignamos el nuevo color a nuestra imagen
+
+            if (esPanel) // Validamos si es un boton perteneciente al panel informativo
+            {
+                brilloIcono.SetActive(true);
+                Color color = imagenIcono.color; // Obtenemos el color actual de la imagen
+                color.a = 1f; // Seteamos el alfa en 1
+                imagenIcono.color = color;
+            }
+
+            if (botonPersonalizacion != null)
+            {
+                botonPersonalizacion.localScale += new Vector3(0.3f, 0.3f, 0.3f);
+            }
         }
     }
 
@@ -34,28 +57,36 @@ public class BrilloBoton : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
         if (imagenBrillo != null)
         {
             imagenBrillo.SetActive(false); // Desactiva el brillo al salir  
+
+            Image imagenActual = this.gameObject.GetComponent<Image>(); // Obtenemos el componenete imagen de nuestro objeto actual
+            Color colorActual = imagenActual.color; // Obtenemos el color actual de la imagen
+            colorActual.a = valorAlfa; // Seteamos el alfa en 1
+            imagenActual.color = colorActual; // Asignamos el nuevo color a nuestra imagen
+
+            if (esPanel)
+            {
+                brilloIcono.SetActive(false);
+                Color color = imagenIcono.color; // Obtenemos el color actual de la imagen
+                color.a = 0.5f; // Seteamos el alfa en 1
+                imagenIcono.color = color;
+            }
+
+            if (botonPersonalizacion != null)
+            {
+                botonPersonalizacion.localScale = posicionInicialBoton;
+            }
         }
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        if (imagenIcono != null)
-        {
-            imagenIcono.SetActive(true);
-            imagenIconoFurtivo.SetActive(true);
+        if (esBoton)
+        {      
+            imagenIcono.gameObject.SetActive(true);
+            imagenIconoFurtivo.gameObject.SetActive(true);
 
-            Image image = imagenIcono.GetComponent<Image>();
-            Image image2 = imagenIconoFurtivo.GetComponent<Image>();
-
-            if (image != null)
-            {
-                image.sprite = icono;
-                image2.sprite = icono;
-            }
-            else
-            {
-                Debug.LogError("El GameObject no tiene un componente Image.");
-            }
+            imagenIcono.sprite = icono;
+            imagenIconoFurtivo.sprite = icono;
         }
     }
 
