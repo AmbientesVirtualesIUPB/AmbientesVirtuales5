005154f5bc12c0e09ab0a239c4edaee3b16c3a04
Laura Cuartas Ríos <l.cuartas96@pascualbravo.edu.co>
2025-01-30 13:44:56 -0500

    controles android

--- Assets/_VE/Scripts/Servidor/MovimientoSexi.cs
+++ Assets/_VE/Scripts/Servidor/MovimientoSexi.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -19,6 +19,42 @@ public class MovimientoSexi : MonoBehaviour
     {
         transform.Translate((Vector3.forward * InputAndroid.GetVeretical1()) * velocidad * Time.deltaTime);
         transform.Rotate(Vector3.up * InputAndroid.GetHorizontal1() * velRotacion * Time.deltaTime) ;
+    }
+
+}*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoSexi : MonoBehaviour
+{
+    public float velocidad = 2f;
+    public float velRotacion = 90f;
+
+    public Joystick joystick; // Referencia al joystick de la UI
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(1);
+        if (!GetComponent<MorionID>().isOwner) enabled = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*transform.Translate((Vector3.forward * InputAndroid.GetVeretical1()) * velocidad * Time.deltaTime);
+        transform.Rotate(Vector3.up * InputAndroid.GetHorizontal1() * velRotacion * Time.deltaTime) ;*/
+        // Obtener los valores del joystick
+        float vertical = joystick.Vertical; // Movimiento hacia adelante y atrás
+        float horizontal = joystick.Horizontal; // Movimiento lateral
+
+        // Movimiento y rotación
+        transform.Translate(Vector3.forward * vertical * velocidad * Time.deltaTime);
+        transform.Rotate(Vector3.up * horizontal * velRotacion * Time.deltaTime);
+
     }
 
 }
